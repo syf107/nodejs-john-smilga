ecbdbf5082d30426f62731c88d9ecb177b98943a
syf107 <syariefarifin107@gmail.com>
2022-04-12 21:06:06 +0700

    commiting the nodejs learning

--- tutorial/app.js
+++ tutorial/app.js
@@ -4,8 +4,10 @@
 const names = require('./4-names');
 const sayHi = require('./5-utils');
 const data = require('./6-alternative-flavor');
-console.log(data);
+require('./7-mind-grenade');
 
-sayHi('Susan');
-sayHi(names.john);
-sayHi(names.peter);
+// console.log(data);
+
+// sayHi('Susan');
+// sayHi(names.john);
+// sayHi(names.peter);
